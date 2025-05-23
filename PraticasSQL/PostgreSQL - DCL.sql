2527d4fd52c5af8e1bac7e04b4a23586aa4bf298
Rhuan Santos <rhuankito@hotmail.com>
2025-05-23 12:52:53 -0300

    Add files via upload

---
+++ PraticasSQL/PostgreSQL - DCL.sql
@@ -0,0 +1,13 @@
+-- Criar um usuario
+-- CREATE USER estagiario WITH PASSWORD '123456';
+
+-- CREATE TABLE empresas(
+--	id SERIAL PRIMARY KEY,
+--	nome VARCHAR(50) NOT NULL,
+--	qnt_funcionarios INT NOT NULL,
+--	cidade VARCHAR(50) NOT NULL
+-- );
+
+
+-- Conceder privilegios para o usuario estagiario
+-- GRANT ALL ON empresas TO estagiario;
