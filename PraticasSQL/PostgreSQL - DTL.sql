0f57324308b4fe0121e07ca752d03083aed6861c
Rhuan Santos <rhuankito@hotmail.com>
2025-05-28 13:58:43 -0300

    Add files via upload

---
+++ PraticasSQL/PostgreSQL - DTL.sql
@@ -0,0 +1,35 @@
+CREATE TABLE tipos_produto(
+	codigo SERIAL PRIMARY KEY,
+	descricao VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE produtos(
+	codigo SERIAL PRIMARY KEY,
+	descricao VARCHAR(50) NOT NULL,
+	preco MONEY NOT NULL,
+	codigo_tipo INT REFERENCES tipos_produto(codigo) NOT NULL
+);
+
+INSERT INTO tipos_produto (descricao) VALUES ('Computador');
+INSERT INTO tipos_produto (descricao) VALUES ('Impressora');
+INSERT INTO tipos_produto (descricao) VALUES ('Notebook');
+
+INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES('Desktop', 1200, 1);
+INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES('Laptop', 1800, 3);
+INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES('Impressora jato de tinta', 510, 2);
+
+BEGIN TRANSACTION;
+
+INSERT INTO tipos_produto (descricao) VALUES ('Equipamentos');
+INSERT INTO tipos_produto (descricao) VALUES ('Nobreak');
+
+-- A transação só finaliza quando o commit é adicionado;
+
+-- Commit
+COMMIT;	
+
+BEGIN TRANSACTION;
+	INSERT INTO tipos_produto (descricao) VALUES ('Tipo A');
+	INSERT INTO tipos_produto (descricao) VALUES ('Tipo B');
+
+ROLLBACK;
